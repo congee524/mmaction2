a190eef40902327bd90fa963d4aef760108afe98
bkhuang <congee524@gmail.com>
2021-03-09 11:01:51 +0800

    lr [10, 15]

--- configs/task/ug2/slowonly_r50_k400_pretrained_8x8x1_256e_ug2_flow.py
+++ configs/task/ug2/slowonly_r50_k400_pretrained_8x8x1_256e_ug2_flow.py
@@ -94,12 +94,12 @@ evaluation = dict(
 
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.015, momentum=0.9,
+    type='SGD', lr=0.03, momentum=0.9,
     weight_decay=0.0001)  # this lr is used for 8 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
-lr_config = dict(policy='step', step=[90, 110])
-total_epochs = 120
+lr_config = dict(policy='step', step=[10, 15])
+total_epochs = 20
 
 # runtime settings
 checkpoint_config = dict(interval=1)
